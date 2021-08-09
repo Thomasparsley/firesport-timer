2a39608d59e000623c8d85e8e2f7d799b738c7a5
Thomasparsley <info@thomasparsley.cz>
2021-08-09 11:27:52 +0200

    project structure fix

--- app/routes.go
+++ app/routes.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/gofiber/fiber/v2"
 
-	"thomasparsley.cz/firesport-timer/routes"
+	"thomasparsley.cz/firesport-timer/app/routes"
 )
 
 func initRoutes(app *fiber.App) {
