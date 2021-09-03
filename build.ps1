3c70520243e8ce0fd5963b23c4484a1b417401e6
Thomasparsley <info@thomasparsley.cz>
2021-09-03 10:37:18 +0200

    build script

---
+++ build.ps1
@@ -0,0 +1,48 @@
+<#
+    This script is used to build the project.
+#>
+
+Set-Location client
+npm install
+if (Test-Path dist) {
+    Remove-Item dist -Recurse -Force
+}
+npm run build
+
+
+Set-Location dist/assets
+Move-Item index.*.css ../../../app/routes
+Move-Item index.*.js ../../../app/routes
+
+Set-Location ../../
+if (Test-Path dist) {
+    Remove-Item dist -Recurse -Force
+}
+
+Set-Location ../app/routes
+if (Test-Path index.css) {
+    Remove-Item index.css
+}
+if (Test-Path index.js) {
+    Remove-Item index.js
+}
+
+Get-ChildItem *.js | Rename-Item -NewName "index.js"
+Get-ChildItem *.css | Rename-Item -NewName "index.css"
+
+
+Set-Location ../
+go build -ldflags -H=windowsgui
+
+Set-Location routes
+if (Test-Path index.css) {
+    Remove-Item index.css
+}
+if (Test-Path index.js) {
+    Remove-Item index.js
+}
+
+Set-Location ../
+Move-Item *.exe ../
+
+Set-Location ../
